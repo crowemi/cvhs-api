0909c80910533bae74373444c87f362c94a98edf
crowemi <crowemi@hotmail.com>
2023-02-24 20:35:50 -0800

    removed public folder

--- public/storage/storage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Storage = void 0;
-class Storage {
-    constructor() { }
-    get_one() { }
-}
-exports.Storage = Storage;
